ce041c2f6b4a2c7f9bcd9053951bae4b5abb87db
ycy <398962898@qq.com>
2017-02-23 15:20:06 +0800

    y

--- src/main/webapp/js/libs/conf.js
+++ src/main/webapp/js/libs/conf.js
@@ -5,19 +5,16 @@ var htmlPath ;
 var adminPath ;
 
 //本项目端口路径
-adminPath = 'http://127.0.0.1:777/qcacg_sys/';
-//adminPath = 'http://127.0.0.1:8090/qcacg_sys/';
+adminPath = 'http://127.0.0.1:8090/qcacg_sys/';
 //adminPath = 'http://121.196.194.211:8080/qcacg_sys/';
 //adminPath = 'http://www.qcacg.com:8080/qcacg_sys/';
 
 //调用跳转项目html域名与端口
 //htmlPath = 'http://127.0.0.1:3000/';
-//htmlPath = 'http://127.0.0.1:8080/';
 htmlPath = 'http://121.196.194.211:8080/';
 //htmlPath = 'http://www.qcacg.com:8080/';
 
 //调用shtml域名与端口
-//rootPath = '/Controller';
 //rootPath = 'http://127.0.0.1:8080/';
 rootPath = 'http://121.196.194.211:8080/Controller/';
 //rootPath = 'http://www.qcacg.com/Controller/';
